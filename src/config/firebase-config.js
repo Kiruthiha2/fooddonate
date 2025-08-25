c1b4baecd4763c37114e241e7c4398e84f4c2c1a
Kiruthihashree <kiruthihashree.s2022cse@sece.ac.in>
2025-08-25 21:28:43 +0530

    first commit

---
+++ src/config/firebase-config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBO3elV076LfV7dxwL-AzMyaFPahPz_GTI",
+  authDomain: "plateshare-debe4.firebaseapp.com",
+  databaseURL: "https://plateshare-debe4-default-rtdb.firebaseio.com",
+  projectId: "plateshare-debe4",
+  storageBucket: "plateshare-debe4.appspot.com",
+  messagingSenderId: "675782368793",
+  appId: "1:675782368793:web:8ccc633cb0619016fe5488"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
